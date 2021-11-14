0ff742e105de3d67a1688a4035a6ef58d2aca2fd
Your Name <you@example.com>
2021-11-14 23:56:47 +0330

    add cart route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,12 @@ import { Route } from "./Classes";
 import HomePage from "../pages/HomePage";
 import NotFound404Page from "../pages/NotFound404Page";
 import ProductsPage from "../pages/ProductsPage";
+import CartPage from "../pages/CartPage";
 
 const routes = [
   new Route("خانه", "/", HomePage),
   new Route("محصولات", "/products", ProductsPage),
+  new Route("سبد خرید شما", "/cart", CartPage),
   new Route("صفحه یافت نشد !", "*", NotFound404Page),
 ];
 
