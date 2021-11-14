c39edd2421c95ff8e16544da41dc4703d10477f1
Your Name <you@example.com>
2021-11-14 23:54:49 +0330

    add products route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
 import { Route } from "./Classes";
 import HomePage from "../pages/HomePage";
 import NotFound404Page from "../pages/NotFound404Page";
+import ProductsPage from "../pages/ProductsPage";
 
 const routes = [
   new Route("خانه", "/", HomePage),
+  new Route("محصولات", "/products", ProductsPage),
   new Route("صفحه یافت نشد !", "*", NotFound404Page),
 ];
 
