11ed464680885014b81b7de4883163e622b093aa
Your Name <you@example.com>
2021-11-14 23:39:21 +0330

    add notification manager util

---
+++ src/utils/notificationManager.js
@@ -0,0 +1,28 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+/**
+ * @description: show notification message to user.
+ *
+ * @param {type} 'success' | 'error' | 'warning' | 'info'
+ * @param {message} string
+ */
+
+export default function notify(type, message) {
+  switch (type) {
+    case "info":
+      toast.info(message);
+      break;
+    case "success":
+      toast.success(message);
+      break;
+    case "warning":
+      toast.warning(message);
+      break;
+    case "error":
+      toast.error(message);
+      break;
+    default:
+      throw new Error("Unknown notification type");
+  }
+}
