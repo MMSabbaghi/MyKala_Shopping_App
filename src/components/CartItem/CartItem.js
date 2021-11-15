73d83159e1fc6b7b7aac9d438f32e0e534ff8122
Your Name <you@example.com>
2021-11-15 16:37:43 +0330

    add increment and decrement cart products

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,8 +1,22 @@
-import { BiCheckShield } from "react-icons/bi";
+import { BiCheckShield, BiMinus, BiPlus, BiTrash } from "react-icons/bi";
 import "./CartItem.css";
-
+import toPersianNumber from "../../utils/toPersianNumber";
+import { useCartDispatch } from "../../context/CartProvider/Provider";
+import {
+  DECREMENT_PRODUCT_QTY,
+  INCREMENT_PRODUCT_QTY,
+} from "../../context/CartProvider/Types";
 const CartItem = ({ product }) => {
-  const { name, image, description } = product;
+  const { name, image, description, quantity, _id } = product;
+  const dispatch = useCartDispatch();
+
+  const incrementQtyHandler = () =>
+    dispatch({ type: INCREMENT_PRODUCT_QTY, payload: _id });
+
+  const decrementQtyHandler = () => {
+    dispatch({ type: DECREMENT_PRODUCT_QTY, payload: _id });
+  };
+
   return (
     <article className="cart_item">
       <img src={image} alt={name} />
@@ -15,6 +29,15 @@ const CartItem = ({ product }) => {
             </li>
           ))}
         </ul>
+        <div className="quantity_btns">
+          <button className="btn" onClick={incrementQtyHandler}>
+            <BiPlus />
+          </button>
+          <span>{toPersianNumber(quantity)}</span>
+          <button className="btn" onClick={decrementQtyHandler}>
+            {quantity > 1 ? <BiMinus /> : <BiTrash />}
+          </button>
+        </div>
       </section>
     </article>
   );
--- src/context/CartProvider/Reducer.js
+++ src/context/CartProvider/Reducer.js
@@ -1,20 +1,40 @@
-import { ADD_TO_CART } from "./Types";
+import {
+  ADD_TO_CART,
+  INCREMENT_PRODUCT_QTY,
+  DECREMENT_PRODUCT_QTY,
+} from "./Types";
 
 const cartReducer = (state, { type, payload }) => {
   switch (type) {
-    case ADD_TO_CART:
+    case ADD_TO_CART: {
       const itemIndex = state.findIndex((p) => p._id === payload._id);
       if (itemIndex < 0) {
         return [...state, { ...payload, quantity: 1 }];
-      }
-      else{
-        const updatedItem = {...state[itemIndex]};
-        updatedItem.quantity +=1;
+      } else {
+        const updatedItem = { ...state[itemIndex] };
+        updatedItem.quantity += 1;
         const newState = [...state];
         newState[itemIndex] = updatedItem;
         return newState;
       }
-
+    }
+    case INCREMENT_PRODUCT_QTY:
+    case DECREMENT_PRODUCT_QTY: {
+      const itemIndex = state.findIndex((p) => p._id === payload);
+      const updatedItem = { ...state[itemIndex] };
+      if (type === DECREMENT_PRODUCT_QTY) {
+        if (updatedItem.quantity > 1) {
+          updatedItem.quantity -= 1;
+        } else {
+          return state.filter((p) => p._id !== payload);
+        }
+      } else {
+        updatedItem.quantity += 1;
+      }
+      const newState = [...state];
+      newState[itemIndex] = updatedItem;
+      return newState;
+    }
     default:
       return state;
   }
