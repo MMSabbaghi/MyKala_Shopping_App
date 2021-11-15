4d59f2b1cd082d7874a6af6dceecc7aab0afc688
Your Name <you@example.com>
2021-11-15 15:30:07 +0330

    refactor price component

--- src/components/CartPrice/CartPrice.js
+++ src/components/CartPrice/CartPrice.js
@@ -5,25 +5,31 @@ import { Link } from "react-router-dom";
 
 const CartPrice = () => {
   const cart = useCart();
-  
+
   const getTotalPrice = () =>
     cart.map((p) => p.price * p.quantity).reduce((p, total) => p + total);
 
+  const getTotalDiscount = () =>
+    cart
+      .map((p) => p.price * p.quantity * p.discount)
+      .reduce((d, total) => d + total) / 100;
+
+  const getFinalPrice = () => getTotalPrice() - getTotalDiscount();
+
   return (
     <section className="cart_price">
-      <p> مجموع قیمت </p>
-      {cart.map(({ name, price, quantity }) => (
-        <div className="row">
-          <p>
-            <span>{name}</span>
-          </p>
-          <p>{toPersianNumber(price * quantity)}$</p>
-        </div>
-      ))}
+      <div className="row">
+        <p>قیمت کالا ها</p>
+        <p>{toPersianNumber(getTotalPrice())}$</p>
+      </div>
+      <div className="row">
+        <p>تخفیف کالا ها</p>
+        <p>{toPersianNumber(getTotalDiscount())}$</p>
+      </div>
       <hr />
       <div className="row">
         <p>مبلغ قابل پرداخت</p>
-        <p>{toPersianNumber(getTotalPrice())}$</p>
+        <p>{toPersianNumber(getFinalPrice())}$</p>
       </div>
       <Link to="/" className="btn btn_primary">
         ادامه سفارش
