0b579d6f2dd1f6ed8b0f54bf83224c7243ccd1d8
Your Name <you@example.com>
2021-11-17 18:39:35 +0330

    add auth provider

--- src/App.js
+++ src/App.js
@@ -3,39 +3,42 @@ import { Route, Routes } from "react-router";
 import routes from "./routes/routes";
 import { ToastContainer } from "react-toastify";
 import CartProvider from "./context/CartProvider/Provider";
+import AuthProvider from "./context/AuthProvider/Provider";
 
 const App = () => {
   return (
     <div className="app">
-      <CartProvider>
-        <Routes>
-          {routes.map(({ title, path, Page, Layout, subRoutes }) => {
-            return (
-              <Route
-                key={path}
-                path={path}
-                element={
-                  <AppPage title={title}>
-                    <Layout>
-                      <Page />
-                    </Layout>
-                  </AppPage>
-                }
-              >
-                {subRoutes.map(({ path: subPath, Element }) => (
-                  <Route
-                    key={path + subPath}
-                    path={subPath}
-                    index={true}
-                    element={<Element />}
-                  />
-                ))}
-              </Route>
-            );
-          })}
-        </Routes>
-      </CartProvider>
-      <ToastContainer />
+      <AuthProvider>
+        <CartProvider>
+          <Routes>
+            {routes.map(({ title, path, Page, Layout, subRoutes }) => {
+              return (
+                <Route
+                  key={path}
+                  path={path}
+                  element={
+                    <AppPage title={title}>
+                      <Layout>
+                        <Page />
+                      </Layout>
+                    </AppPage>
+                  }
+                >
+                  {subRoutes.map(({ path: subPath, Element }) => (
+                    <Route
+                      key={path + subPath}
+                      path={subPath}
+                      index={true}
+                      element={<Element />}
+                    />
+                  ))}
+                </Route>
+              );
+            })}
+          </Routes>
+        </CartProvider>
+        <ToastContainer />
+      </AuthProvider>
     </div>
   );
 };
---
+++ src/context/AuthProvider/Provider.js
@@ -0,0 +1,29 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+const AuthContextDispatcher = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(null);
+
+  useEffect(() => {
+    const authData = localStorage.getItem("authState");
+    setAuth(JSON.parse(authData));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("authState", JSON.stringify(auth));
+  }, [auth]);
+
+  return (
+    <AuthContext.Provider value={auth}>
+      <AuthContextDispatcher.Provider value={setAuth}>
+        {children}
+      </AuthContextDispatcher.Provider>
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
+export const useAuth = () => useContext(AuthContext);
+export const useSetAuth = () => useContext(AuthContextDispatcher);
