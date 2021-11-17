5ded6d22698aa59d7db0a7d8afa5f2b65c50acab
Your Name <you@example.com>
2021-11-17 13:23:02 +0330

    add login service

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -3,3 +3,7 @@ import http from "./httpServices";
 export function signUpUser(userData) {
   return http.post("/user/register", userData);
 }
+
+export function loginUser(loginData){
+  return http.post("/user/login" , loginData);
+}
