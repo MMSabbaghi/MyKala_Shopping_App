3b3d1b56d830c374dfce7eaca1d1ecb62e0f47b6
MMSabbaghi <mmsabbaghi1399@gmail.com>
2021-11-18 21:50:17 +0330

    add overview section for home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,24 @@
 import Products from "../components/Products/Products";
+import shoppingImg from "../assets/images/shoping.gif";
+import "./styles/HomePage.css";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div className="container">
-      <Products />
-    </div>
+    <>
+      <section className="overview">
+        <div className="container">
+          <img src={shoppingImg} alt="shopping" />
+          <div className="desc">
+            <h2> فروشگاه اینترنتی مای کالا </h2>
+            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ،</p>
+            <Link to="/products" className="btn btn_primary">
+              مشاهده محصولات
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
