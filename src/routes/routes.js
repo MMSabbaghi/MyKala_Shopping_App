386b00d8f5abc2c858ab419e957ed43c15fb1f51
MMSabbaghi <mmsabbaghi1399@gmail.com>
2021-11-22 16:42:07 +0330

    add products page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,10 +7,12 @@ import LoginForm from "../components/AuthForms/LoginForm";
 import SignupForm from "../components/AuthForms/SignupForm";
 import AuthLayout from "../layout/AuthLayout";
 import ProfilePage from "../pages/ProfilePage";
+import ProductsPage from "../pages/ProductsPage";
 
 const routes = [
   new Route("خانه", "/", HomePage),
   new Route("سبد خرید شما", "/cart", CartPage),
+  new Route("محصولات", "/products", ProductsPage),
   new Route("ورود", "/login", LoginForm, { layout: AuthLayout }),
   new Route("ثبت نام", "/signup", SignupForm, { layout: AuthLayout }),
   new Route("ثبت سفارش", "/checkout", CheckOutPage, { authorize: true }),
