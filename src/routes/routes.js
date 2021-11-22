cc2fca1d18fbe16dba15c27769f7274e5cac3b00
MMSabbaghi <mmsabbaghi1399@gmail.com>
2021-11-22 12:30:22 +0330

    remove products page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,6 @@
 import { Route } from "./Classes";
 import HomePage from "../pages/HomePage";
 import NotFound404Page from "../pages/NotFound404Page";
-import ProductsPage from "../pages/ProductsPage";
 import CartPage from "../pages/CartPage";
 import CheckOutPage from "../pages/CheckOutPage";
 import LoginForm from "../components/AuthForms/LoginForm";
@@ -11,7 +10,6 @@ import ProfilePage from "../pages/ProfilePage";
 
 const routes = [
   new Route("خانه", "/", HomePage),
-  new Route("محصولات", "/products", ProductsPage),
   new Route("سبد خرید شما", "/cart", CartPage),
   new Route("ورود", "/login", LoginForm, { layout: AuthLayout }),
   new Route("ثبت نام", "/signup", SignupForm, { layout: AuthLayout }),
