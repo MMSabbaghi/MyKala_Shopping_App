fa2e6b43def05d752bcbe0f5674c2a15c381e426
MMSabbaghi <mmsabbaghi1399@gmail.com>
2021-11-23 15:18:19 +0330

    add loading prop

--- src/components/common/Loader/Loader.js
+++ src/components/common/Loader/Loader.js
@@ -1,6 +1,7 @@
 import styles from "./Loader.module.css";
 
-const Loader = ({ loading, size = 60 }) => {
+const Loader = ({ loading }) => {
+  if (!loading) return null;
   return (
     <div className={styles.loader}>
       <div className={styles.spinner}></div>
