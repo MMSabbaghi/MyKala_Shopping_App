5fbfc63a25c67f4c1f25cd1e6303192c621478f0
MMSabbaghi <mmsabbaghi1399@gmail.com>
2021-11-23 12:38:05 +0330

    change loader

--- src/components/common/Loader/Loader.js
+++ src/components/common/Loader/Loader.js
@@ -1,10 +1,9 @@
-import { PuffLoader } from "react-spinners";
 import styles from "./Loader.module.css";
 
 const Loader = ({ loading, size = 60 }) => {
   return (
     <div className={styles.loader}>
-      <PuffLoader loading={loading} color="var(--primary-color)" size={size} />
+      <div className={styles.spinner}></div>
     </div>
   );
 };
