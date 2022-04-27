850ea378fa7b288bfcd61f165eeb2fdd9e562093
MMSabbaghi <mmsabbaghi1399@gmail.com>
2022-04-28 04:06:12 +0430

    add tilwind css.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+    "body": "#eee",
+    "primary": "#4338ca",
+    "secondary": "#fff",
+    "red": "#f44336",
+    "gray": "#ccc",
+    "gray-2": "#888"
+    },
+  },
+  plugins: [],
+}
