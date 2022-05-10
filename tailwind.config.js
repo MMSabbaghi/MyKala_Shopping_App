e1b66c2e10d14387971de4b38ff94fe197d8b2df
MMSabbaghi <mmsabbaghi1399@gmail.com>
2022-05-10 17:08:20 +0430

    change tailwind configs to use tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-    "body": "#eee",
-    "primary": "#4338ca",
-    "secondary": "#fff",
-    "red": "#f44336",
-    "gray": "#ccc",
-    "gray-2": "#888"
+    extend: {
+      colors: {
+        body: "#eee",
+        primary: "#4338ca",
+        secondary: "#fff",
+        red: "#f44336",
+        gray: "#ccc",
+        "gray-2": "#888",
+      },
     },
   },
   plugins: [],
-}
+};
